7efb879849d594e83df7401dbf9027ed3e826c72
adunseayorinde <adunseayorinde@gmail.com>
2025-07-02 22:58:03 +0100

    fixed state property name in comparison_agent

--- agents/comparison_agent.py
+++ agents/comparison_agent.py
@@ -2,7 +2,7 @@ from typing import Dict, List
 from tools.comparison_tool import run_comparison_tool
 
 def run(state: Dict) -> Dict:
-    current_analysis = state.get("anaylsis_result", "")
+    current_analysis = state.get("analysis_result", "")
     current_trends = state.get("aggregated_trends", "")
     comparison = state.get("comparison_target", "")
 
